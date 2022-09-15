feae9b53e20e676e33b3c1e169479c25d5c884a8
Armada Afga Adrian Nasar <armada.nasar@pinhome.id>
2022-09-15 19:57:56 +0700

    feat: add fetching data

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const startFetchDataBtn = document.getElementById("startFetchDataBtn")
+
+let rentedCarsData = []
+
+startFetchDataBtn.addEventListener("click", fetchRentCarsDataAsyncAwait)
+
+function fetchRentCarsData() {
+    fetch('https://bootcamp-rent-car.herokuapp.com/admin/car').
+        then(result => result.json()).
+        then((result) => {
+            rentedCarsData = result
+        }).
+        catch(err => console.error(err))
+}
+
+async function fetchRentCarsDataAsyncAwait() {
+    try {
+        let result = await fetch('https://bootcamp-rent-car.herokuapp.com/admin/car')
+        let rentCars = await result.json()
+
+        rentedCarsData = rentCars
+
+    } catch (err) {
+        console.error(err)
+    }
+}
